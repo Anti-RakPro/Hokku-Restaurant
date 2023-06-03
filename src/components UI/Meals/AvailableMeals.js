9570800dd6cecc10acb24f1e1acbc0f28ebe23e3
kvit.d.o@gmail.com <kvit.d.o@gmail.com>
2023-06-03 13:13:58 +0300

    init: fetch datta from fierbase

--- src/components UI/Meals/AvailableMeals.js
+++ src/components UI/Meals/AvailableMeals.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import Card from '../UI/Card'
+
 
 
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+const DUMMY_MEALS = []
 const  AvailableMeals = ()=>{
+    const [meals, setMeals] = useState([]);
 
-     const mealsList = DUMMY_MEALS.map(meal => {
+    useEffect( ()=>{
+        const fetchMeals = async () =>{
+           const response = await fetch('https://react-http-753dd-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+           const responseData = await response.json();
+            console.log(responseData)
+           const loadedMeals = []
+
+            for (const key in responseData){
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                })
+            }
+            setMeals(loadedMeals)
+        }
+        fetchMeals()
+    }, []);
+
+
+     const mealsList = meals.map(meal => {
          return (
              <MealItem key={meal.id}  id={meal.id} name={meal.name} description = {meal.description} price={meal.price}>{meal.name} </MealItem>
          )
